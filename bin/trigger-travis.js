3156e034dca70d85ee5702c5db6c060a6aaeb451
ishmael.ahmed <ishmael.ahmed@readingplus.com>
2017-04-25 14:25:25 -0400

    Moving command line parse here

--- bin/trigger-travis.js
+++ bin/trigger-travis.js
@@ -1,2 +1,46 @@
 #!/usr/bin/env node
-require('../');
+
+const pkginfo = require('pkginfo')(module),
+    trigger = require('../lib/trigger'),
+    ArgumentParser = require('argparse').ArgumentParser,
+    parser = new ArgumentParser({
+        version: this.version,
+        addHelp: true,
+        description: this.description
+    });
+
+//args
+
+
+parser.addArgument(['-r', '--repo'], {
+    help: 'The name of the repo e.g. ishmael-readingplus/[trigger-travis]',
+    required: true
+});
+parser.addArgument(['-o', '--owner'], {
+    help: 'The owner of the github repo e.g [ishmael-readingplus]/trigger-travis',
+    required: true
+});
+parser.addArgument(['-t', '--token'], {
+    help: 'Travis api token',
+    required: true
+});
+parser.addArgument(['-b', '--branch'], {
+    help: 'The name of the branch to trigger by default it uses the upstream master branch',
+    defaultValue: 'master'
+});
+parser.addArgument(['--pro'], {
+    help: 'Set if the repo is on github.com the default is github.org',
+    defaultValue: false,
+    action: 'storeTrue',
+    type: Boolean
+});
+parser.addArgument(['--debug'], {
+    help: 'Prints message to the console, but takes no action',
+    defaultValue: false,
+    action: 'storeTrue',
+    type: Boolean
+});
+
+var argv = parser.parseArgs();
+console.log(argv)
+trigger.pull(argv)
